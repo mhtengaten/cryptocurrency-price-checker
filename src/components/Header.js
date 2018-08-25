8504ff5966e45f49ca387cd648eb678443434933
mhtengaten <mhtengaten@gmail.com>
2018-08-25 13:05:13 +0200

    Creating Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Header = (props) => {
+    return (
+        <View>
+            <Text>Cyrptocurrency App</Text>
+        </View>
+    )
+}
+
+export default Header;
