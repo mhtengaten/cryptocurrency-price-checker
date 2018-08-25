0aad1501805d1ea94b29d85664fcc8a0d140fe5a
mhtengaten <mhtengaten@gmail.com>
2018-08-25 13:14:22 +0200

    styling header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-
-const Header = (props) => {
+const Header = () => {
     return (
-        <View>
-            <Text>Cyrptocurrency App</Text>
+        <View style={headerContainer}>
+            <Text style={header}>
+                Cyrptocurrency App
+            </Text>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        display: "flex",
+        marginTop: 55,
+        alignItems: "center"
+    },
+    header: {
+        fontWeight: "bold",
+        fontSize: 20
+    }
+});
+
+const { headerContainer, header } = styles;
 
 export default Header;
